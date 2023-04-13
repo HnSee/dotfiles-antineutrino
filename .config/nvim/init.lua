e5fc844e86e42a9890324f0ffed8a73ba00b9f06
HnSee <34213265+HnSee@users.noreply.github.com>
2023-04-13 12:09:18 +0200

    Add auto remove of background colors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,14 +25,16 @@ require("lazy").setup({
     lazy = false,
     priority = 1000,
     config = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
+          vim.cmd("highlight NonText ctermbg=NONE guibg=NONE")
+          vim.cmd("highlight LineNr ctermbg=NONE guibg=NONE")
+        end
+      })
       vim.cmd([[colorscheme oxocarbon]])
     end,
-    dependencies = {
-      {
-        "shaun-mathew/Chameleon.nvim",
-        config = true,
-      }
-    }
   },
   {
     "nvim-lualine/lualine.nvim",
